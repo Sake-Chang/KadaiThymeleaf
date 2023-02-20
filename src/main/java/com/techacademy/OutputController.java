cc43744fbaa63e5af654556b4c269a5aa45f9dea
hideaki.sakai <hideaki.sakai.75@gmail.com>
2023-02-20 19:13:49 +0900

    Lesson15課題 再提出

--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -10,9 +10,7 @@ public class OutputController {
 
     @PostMapping("/output")
     public String postOutput(@RequestParam("val") String val, Model model) {
-        // フォームから送信されてきた値をModelに登録
         model.addAttribute("val", val);
-        // output.htmlに画面遷移
         return "output";
     }
 }
